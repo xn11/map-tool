56517cf176390140fbc6487cfc7463d77d020106
Xu <I314083@global.corp.sap>
2016-03-14 18:16:11 +0800

    add config file

---
+++ config.js
@@ -0,0 +1,85 @@
+var config = {
+
+	//底图列表，默认显示第一个
+	"baseMaps":[
+		{
+			"name":"osm",
+			"url":"http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+		},
+		{
+			"name":"osmde",
+			"url":"http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+		},
+		{
+			"name":"local",
+			"url":"mapNanjingDark3/{z}/{x}/{y}.png"
+		}
+	],
+
+	//导航服务来源
+	"webServices":
+		{
+			"baidu": {
+				"url": "http://api.map.baidu.com/direction/v1?",		//请求服务的url
+				"key": "rnFk0NhFaSRv7b6rXH1dpNAN",
+				"type": ["driving"]
+			},
+			"lbs": {
+				"url": "http://webapi.amap.com/maps?v=1.3&key=2ad58dd1832ce97111bf2f62921a968c",
+				"key": "2ad58dd1832ce97111bf2f62921a968c",
+				"type": ["Driving","Walking"]
+			},
+			"hana": {
+				"hostname": "127.0.0.1",
+				"port": 1337
+			},
+			"osrm": {
+				"url": "",
+				"key": ""
+			},
+			
+		},	
+
+	//页面提供的导航选项
+	"routings":[
+		{
+			"name": "高德驾车导航",
+			"id": "lbs-driving",
+			"provider": "lbs",
+			"type": 0
+		},
+		{
+			"name": "高德步行导航",
+			"id": "lbs-walking",
+			"provider": "lbs",
+			"type": 1
+		},
+		{
+			"name": "百度导航",
+			"id": "baidu",
+			"provider": "baidu",
+			"type": 0,
+			"options": {
+				"origin_region": "南京",
+				"destination_region": "南京",
+				"tactics": 12,			//导航路线类型:10-不走高速；11-最少时间；12-最短路径(默认)
+			}
+		},
+		{
+			"name": "HANA导航",	
+			"id": "hana",
+			"provider": "hana"
+		},
+		{
+			"name": "OSRM导航",
+			"id": "osrm",
+			"provider": "osrm"
+		}
+	],
+
+	
+
+	"centerPoint":[32.045115, 118.778601],		//地图显示的中心点
+
+	"zoom": 13,			//初始的地图缩放比例
+};
